e2e2ca9b4830afe0661d6f9aff1ed9b873d3dea8
Nikita <nickdanse99@gmail.com>
2021-08-30 06:03:31 +0300

    cheburek

---
+++ js/script.js
@@ -0,0 +1,73 @@
+"use strict";
+
+// let number = 5;
+// const LeftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const person = " 5 like";
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: "Jhon",
+//     age: 12,
+//     isMaried: false 
+// };
+
+// console.log(obj.age); 
+
+// const arr = ['apple.png', 'music.mp3', 5, {}, []];
+// console.log(arr[1]);
+
+// alert ('Hi cheburek');
+
+// const result = confirm("Are u here?");
+// console.log(result);
+
+// const answer = +prompt("Как тебя зовут?","");
+// console.log(answer + 5);
+
+// const answers = [];
+// answers[0] = prompt("Как тебя зовут?");
+// answers[1] = prompt("Какая у тебя фамилия?");
+// answers[2] = prompt("Дата твоего рождения?","13.10.1999");
+
+// // document.write(answers); 
+// console.log(typeof(answers)); 
+// console.log(typeof(null));
+
+// const name = 'Jhon';
+
+// console.log('https://proto.col/' + name + '/com');
+
+// console.log(`http://protocol/man/${name}/com`);
+
+// const lastname = 'Piska';
+
+// alert(`Салам на пополам, ${lastname}`);
+
+console.log(4 + +"4");
+
+let incr = 10,
+    decr = 10;
+// incr++;
+// decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2+2*2 !== '6');
+
+
+const isCheckit = false,
+      isClosed = false;
+    //   isOpen = true; 
+
+console.log(isCheckit || !isClosed);
+
+
---
+++ js/script1.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const name = 'Jhon';
+
+// console.log('https://proto.col/' + name + '/com');
+
+console.log(`http://protocol/man/${name}/com`);
+
+const lastname = 'Piska';
+
+alert(`Салам на пополам, ${lastname}`);
